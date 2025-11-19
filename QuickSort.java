009dff1a0e3261121d7e6abaf980f3d6bad3fab3
rcriss266 <rcriss266@gmail.com>
2025-11-18 20:16:08 -0600

    método imprimirArreglo

--- QuickSort.java
+++ QuickSort.java
@@ -3,5 +3,13 @@ public class QuickSort {
     public static void main(String[] args) {
 
     }
+
+    public static void imprimirArreglo(int[] arr) {
+    for (int num  arr) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+}
+
 }
 
