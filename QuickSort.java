547e2cb494bd19a9dc43b883c0e2c0d67fcb99a5
rcriss266 <rcriss266@gmail.com>
2025-11-18 20:22:13 -0600

    Agregar método quickSort y particion

--- QuickSort.java
+++ QuickSort.java
@@ -1,15 +1,39 @@
 public class QuickSort {
 
+    public static void quickSort(int[] arr, int inicio, int fin) {
+        if (inicio < fin) {
+            int indicePivote = particion(arr, inicio, fin);
+            quickSort(arr, inicio, indicePivote - 1);
+            quickSort(arr, indicePivote + 1, fin);
+        }
+    }
+
+    public static int particion(int[] arr, int inicio, int fin) {
+        int pivote = arr[fin];
+        int i = inicio - 1;
+
+        for (int j = inicio; j < fin; j++) {
+            if (arr[j] <= pivote) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[fin];
+        arr[fin] = temp;
+
+        return i + 1;
+    }
+
     public static void main(String[] args) {
 
     }
-
-    public static void imprimirArreglo(int[] arr) {
-    for (int num : arr) {
-        System.out.print(num + " ");
-    }
-    System.out.println();
 }
 
-}
+
+
+
 
